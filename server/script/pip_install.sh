c208cd28bbe0f149d9e4cfc607793994d5cbf7dd
satoshi_shiba <satoshi_shiba@sidefeed.com>
2025-07-10 22:28:06 +0900

    install script を調整

--- server/script/pip_install.sh
+++ server/script/pip_install.sh
@@ -2,5 +2,5 @@
 
 set -eux -o pipefail
 
-pip install -r requirements.txt \
+pip3 install -r requirements.txt \
   --index-url https://repo.fury.io/emoji-gen/ --extra-index-url https://pypi.org/simple
