836050d2981e9ca031b3e13d032eb5abab998269
satoshi_shiba <satoshi_shiba@sidefeed.com>
2025-07-11 18:03:33 +0900

    使う予定の無いファイルやディレクトリを削除

--- server/src/config.py
+++ server/src/config.py
@@ -8,25 +8,8 @@ import yaml
 class Config():
     def __init__(self, *, is_dev):
         self.path_config = PathConfig()
-        self.app_config = AppConfig(path_config=self.path_config, is_dev=is_dev)
         self.fonts_config = FontsConfig(path_config=self.path_config)
         self.locales_config = LocalesConfig()
-
-
-# ---------------------------------------------------------
-# App
-# ---------------------------------------------------------
-
-class AppConfig():
-    def __init__(self, *, path_config, is_dev):
-        dev_config_path = path_config.config_path.joinpath('app-dev.yml')
-        prod_config_path = path_config.config_path.joinpath('app-prod.yml')
-        config_path = dev_config_path if is_dev else prod_config_path
-
-        with open(config_path, 'r', encoding='utf-8') as fp:
-            config = yaml.full_load(fp)
-
-        self.site_url = config['site_url']
 
 
 # ---------------------------------------------------------
